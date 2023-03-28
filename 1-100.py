70128b352cb1f85e3a2b51a5ed7941d40f2ec03e
unknown <qortmdals94@naver.com>
2023-03-28 15:38:13 +0900

    230328

---
+++ 1-100.py
@@ -0,0 +1,8 @@
+oddlist = list(range(1, 101, 2))
+sum = 0
+for i in range(100):
+    if i % 2 == 1 :
+        sum += i
+    
+
+print(sum)
---
+++ banksum.py
@@ -0,0 +1,7 @@
+arr= {127, 890, 57, 80, 1, 200, 170}
+sum = 0
+
+for a in arr :
+    sum += a
+
+print(sum)
